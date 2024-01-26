65666b9dca3d7c7f0d4c431b9584e473c52c720f
Olga <olga.sepulveda.berrios@gmail.com>
2024-01-26 11:59:49 -0300

    mensaje

---
+++ assets/js/index.js
@@ -0,0 +1,39 @@
+
+
+  $(document).ready(function(){
+    $("#enviar").click(function(){
+      alert("¡Nos contactaremos con usted a la brevedad!");
+    });
+
+
+  });
+
+
+
+
+  
+/*  const myCarouselElement = document.querySelector("#carouselExampleIndicators");
+
+const carousel = new bootstrap.Carousel(myCarouselElement, {
+  interval: 2000,
+  touch: false,
+});
+
+$(document).ready(function () {
+  $("#enviarCorreo").click(function () {
+    alert("El correo fue enviado correctamente...");
+  });
+
+  $("h4").click(function () {
+    $("p.card-text1").toggle();
+  });
+
+  $("#titulo1").on("dblclick", function () {
+    $("#titulo1").css("color", "red");
+  });
+
+  $("#titulo2").on("dblclick", function () {
+    $("#titulo2").css("color", "red");
+  });
+});
+ */
